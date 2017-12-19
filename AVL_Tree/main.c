515fbd728b77df2890f02239146a866794f16132
Gnomeek <gnomeek@Outlook.com>
2017-12-19 23:42:59 +0800

    20171219

--- AVL_Tree/main.c
+++ AVL_Tree/main.c
@@ -27,8 +27,8 @@ int AvlTree_Height(AvlTree T){
 }
 
 /*比较取最大树高*/
-int MAX(AvlTree T1,AvlTree T2){
-    return T1->height>T2->height ? T1->height : T2->height;
+int MAX(int T1,int T2){
+    return T1>T2 ? T1 : T2;
 }
 
 /*寻找最小结点*/
@@ -79,6 +79,20 @@ AvlTree DoubleRotate_Left(AvlTree T3){
 AvlTree DoubleRotate_Right(AvlTree T3){
     T3->right=SingleRotate_Left(T3->right);
     return SingleRotate_Right(T3);
+}
+
+/*创建结点*/
+AvlTree AvlTree_Create(ElementType x,AvlNode *left,AvlNode *right){
+    AvlTree p;
+
+    p=(AvlNode*)malloc(sizeof(AvlNode));
+    if(NULL==p)
+        return NULL;
+    p->element=x;
+    p->left=left;
+    p->right=right;
+    p->height=0;
+    return p;
 }
 
 /*插入结点*/
@@ -168,105 +182,107 @@ AvlTree AvlTree_Find(ElementType x,AvlTree T){
         return AvlTree_Find(x,T->right);
 }
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-int main()
-{
-    printf("Hello world!\n");
-    return 0;
+/*先序遍历*/
+Status AvlTree_PreOrderTraverse(AvlTree T){
+    if(NULL==T)
+        return OK;
+    if(T!=NULL){
+        printf("%d",T->element);
+        AvlTree_PreOrderTraverse(T->left);
+        AvlTree_PreOrderTraverse(T->right);
+    }
 }
+
+/*中序遍历*/
+Status AvlTree_InOrderTraverse(AvlTree T){
+    if(NULL==T)
+        return OK;
+    if(T!=NULL){
+        AvlTree_PreOrderTraverse(T->left);
+        printf("%d",T->element);
+        AvlTree_PreOrderTraverse(T->right);
+    }
+}
+
+/*后序遍历*/
+Status AvlTree_PostOrderTraverse(AvlTree T){
+    if(NULL==T)
+        return OK;
+    if(T!=NULL){
+        AvlTree_PreOrderTraverse(T->left);
+        AvlTree_PreOrderTraverse(T->right);
+        printf("%d",T->element);
+    }
+}
+
+/*销毁*/
+Status AvlTree_Destroy(AvlTree T){
+    if (NULL==T)
+        return ERROR;
+    if (T->left != NULL)
+        AvlTree_Destroy(T->left);
+    if (T->right != NULL)
+        AvlTree_Destroy(T->right);
+    free(T);
+}
+
+/*打印*/
+Status AvlTree_Print(AvlTree T, ElementType x, int direction)
+{
+    if(T!= NULL)
+    {
+        if(direction==0)    // tree是根节点
+            printf("%2d is root\n", T->element);
+        else                // tree是分支节点
+            printf("%2d is %2d's %6s child\n",T->element,x, direction==1?"right" : "left");
+
+        AvlTree_Print(T->left,T->element,-1);
+        AvlTree_Print(T->right,T->element,1);
+    }
+    return OK;
+}
+
+int main(){
+    int i;
+    AvlTree root=NULL;
+    int arr[16]= {3,2,1,4,5,6,7,16,15,14,13,12,11,10,8,9};
+    printf("== 高度: %d\n", AvlTree_Height(root));
+    printf("== 依次添加: ");
+    int ilen = 15;
+    for(i=0; i<ilen; i++)
+    {
+        printf("%d ", arr[i]);
+        root = AvlTree_Insert(arr[i],root);
+    }
+
+    printf("\n== 前序遍历: ");
+    AvlTree_PreOrderTraverse(root);
+
+    printf("\n== 中序遍历: ");
+    AvlTree_InOrderTraverse(root);
+
+    printf("\n== 后序遍历: ");
+    AvlTree_PostOrderTraverse(root);
+    printf("\n");
+
+    printf("== 高度: %d\n", AvlTree_Height(root));
+    printf("== 最小值: %d\n", AvlTree_Max(root)->element);
+    printf("== 最大值: %d\n", AvlTree_Min(root)->element);
+    printf("== 树的详细信息: \n");
+    AvlTree_Print(root, root->element, 0);
+
+
+    int j = 8;
+    printf("\n== 删除根节点: %d", j);
+    root = AvlTree_Delete(j,root);
+
+    printf("\n== 高度: %d", AvlTree_Height(root));
+    printf("\n== 中序遍历: ");
+    AvlTree_InOrderTraverse(root);
+    printf("\n== 树的详细信息: \n");
+    AvlTree_Print(root, root->element, 0);
+
+    // 销毁二叉树
+    AvlTree_Destroy(root);
+}
+
